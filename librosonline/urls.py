d0794d17575ad048c771cec3c06624dc3cea7bff
Agus <agu.pala@gmail.com>
2019-07-26 20:47:08 -0300

    Segundo commit

--- librosonline/urls.py
+++ librosonline/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vistaprevia/', include('vistaprevia.urls')),
+    # path('', include('vistaprevia.urls')),
 ]
