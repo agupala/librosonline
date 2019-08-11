81032ff427518bcfbf4b2ebdbecbc1bf9b08a038
agupala <agu.pala@gmail.com>
2019-08-11 02:34:40 -0300

    Hasta style.css

--- vistaprevia/views.py
+++ vistaprevia/views.py
@@ -6,6 +6,18 @@ from django.template import RequestContext, loader
 
 def index(request):
     # return HttpResponse("Hola Mundo!!!")
-    contenido = {'nombre_sitio': 'LibrosOnline'}
-    return render(request, 'vistaprevia/index.html', contenido)
+    contenido = {
+        'nombre_sitio': 'LibrosOnline'
+    }
+    para_minorista = {
+        'tipo_usuario':'minorista', 
+        'incremento': '25'
+    }
+    para_mayorista = {'tipo_usuario':'mayorista', 'incremento': '10'}
+    # page_title = "Titulo de Pagina"
+    # site_name = "Venta de Libros"
+    # palabras_clave = "Palabras clave"
+    # descripcion = "Descripcion"
+
+    return render(request, 'vistaprevia/index.html', {'contenido':contenido, 'para_minorista':para_minorista, 'para_mayorista':para_mayorista})
 
