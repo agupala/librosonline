8dd8e54edcf95db654de06435a8b73240e9729ea
Agus <agu.pala@gmail.com>
2019-08-24 13:10:03 -0300

    Practica V

--- vistaprevia/admin.py
+++ vistaprevia/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from vistaprevia.models import Categoria, Producto
 
 # Register your models here.
+def publicar(modeladmin, request, queryset):
+    queryset.update(estado='Publicado')
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    fields = ['categoria', 'fecha_publicacion', 'producto', 'ruta_imagen', 'estado']
+    list_display = ['producto', 'fecha_publicacion', 'ruta_imagen', 'tipo_de_producto']
+    ordering = ['-fecha_publicacion']
+    list_filter = ('producto', 'fecha_publicacion')
+    actions = [publicar]
+    
+
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Categoria)
